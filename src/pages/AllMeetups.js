ae893e6a07d77ec7c7c544d30284d37e09ef47f5
codenamerush <aarushisharda@gmail.com>
2022-08-10 00:35:43 +0530

    adding nested components

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,7 +1,47 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_DATA = [
+  {
+    id: "1",
+    title: "First",
+    image:
+      "https://via.placeholder.com/150",
+    address: "dummy",
+    description: "text",
+  },
+  {
+    id: "2",
+    title: "Second",
+    image:
+      "https://via.placeholder.com/150",
+    address: "dummy",
+    description: "text",
+  },
+  {
+    id: "3",
+    title: "Third",
+    image:
+      "https://via.placeholder.com/150",
+    address: "dummy",
+    description: "text",
+  },
+  {
+    id: "4",
+    title: "Fourth",
+    image:
+      "https://via.placeholder.com/150",
+    address: "dummy",
+    description: "text",
+  },
+];
+
 function AllMeetupsPage() {
-    return (
-        <div>All meetups page</div>
-    )
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={DUMMY_DATA} />;
+    </section>
+  );
 }
 
 export default AllMeetupsPage;
